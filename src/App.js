58e2547b2238dde52b2182ead14fdb2f98c76d9c
Volodymyr Grebeniuk <grebenyukvd@gmail.com>
2023-09-08 10:08:51 +0300

    signIn

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Header from "./components/header";
 import Footer from "./components/footer";
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Main from "./components/main";
-// import Reg from "./UI/reg"
+import Reg from "./UI/reg"
 import Login from "./UI/login"
-import React from "react";
+
 export default () => {
     return (
         <div className="app">
