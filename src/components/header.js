248a97fea4e8ed13a7d976d7b89c10a7d4d5a6b6
Volodymyr Grebeniuk <grebenyukvd@gmail.com>
2023-09-13 14:47:06 +0300

    del link to counters fm header

--- src/components/header.js
+++ src/components/header.js
@@ -48,9 +48,6 @@ const Header = () => {
                         <li>
                             <NavLink className="py-2 pl-3 pr-4 text-sm text-white bg-black rounded md:bg-transparent md:p-0 md:dark:text-black" to="cabinet">Особистий кабінет</NavLink>
                         </li>
-                        <li>
-                            <NavLink className="py-2 pl-3 pr-4 text-sm text-white bg-black rounded md:bg-transparent md:p-0 md:dark:text-black" to="counters">Лічильники</NavLink>
-                        </li>
                     </ul>
                 </div>
             </div>
