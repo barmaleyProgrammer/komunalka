459f7a3a9cf5d0580bb1ae91f02361915b27ddfc
Volodymyr Grebeniuk <grebenyukvd@gmail.com>
2023-10-02 18:01:28 +0300

    pop-up resetPassword email confirmation commit 3

--- src/pages/auth/resetPassword.js
+++ src/pages/auth/resetPassword.js
@@ -52,7 +52,7 @@ const ResetPasswordRequest = () => {
                         <p className="text-left text-base">Будь ласка, перевірте свою електронну пошту та підтвердіть свою реєстрацію,
                             щоб розпочати користуватися нашим сервісом. Не забудьте перевірити папку «Спам»,
                             якщо лист не зʼявиться в основній скринці.<br/> З повагою, команда LYCHYLNYK.</p>
-                        <p className="text-left text-base mt-4"><u>Надіслати мені лист повторно</u></p>
+                        <p className="text-left text-base mt-4 underline underline-offset-4">Надіслати мені лист повторно</p>
                     </div>
                     <div className="pt-2 w-44 mx-auto">
                         <Button type="button" label={'Ok'} cssType={'primary'} onClick={() => setModalActive(false)} />
