1c9ede888045c68d6ea9f7c4faf51c8c0652641b
Volodymyr Grebeniuk <grebenyukvd@gmail.com>
2023-10-02 17:44:57 +0300

    pop-up resetPassword email confirmation

--- src/pages/auth/resetPassword.js
+++ src/pages/auth/resetPassword.js
@@ -1,11 +1,15 @@
 import { useState } from "react";
 import InputField from "../../components/inputField";
+import { useNavigate } from "react-router-dom";
 import logo_lichylnyk from "../../img/logo_lichylnyk.svg";
 import Button from "../../components/button";
 import api from "../../api";
+import Modal from "../../components/modal/modal";
 
 const ResetPasswordRequest = () => {
     const [validateFlag, setValidateFlag] = useState(false);
+    const [modalActive, setModalActive] = useState(false);
+    const navigate = useNavigate();
     const [form, setForm] = useState({
         email: (process.env.NODE_ENV === 'development') ? 'grebenyukvd@gmail.com' : '',
         rememberMe: false
@@ -26,7 +30,9 @@ const ResetPasswordRequest = () => {
             const result = await api.resetPasswordRequest(form.email);
             if (result.status === 200) {
                 setValidateFlag(true);
+                setModalActive(true);
             }
+            // navigate('/');
         }
         catch (e) {
                 setFormError(e);
@@ -39,7 +45,20 @@ const ResetPasswordRequest = () => {
             <h4 className="text-black_figma text-center">Відновлення пароля</h4>
             <div className="text-xs text-red-900 text-center">{formError}</div>
             { validateFlag ?
-                <div className="text-xs text-blue-900 text-center">На вказану адресу вiдправлено листа з посиланням</div> :
+                // <div className="text-xs text-blue-900 text-center">На вказану адресу вiдправлено листа з посиланням</div> :
+                <Modal active={modalActive} setActive={setModalActive}>
+                    <div className="text-xs text-black_figma p-5 text-center w-[572px] h-auto">
+                        <h1 className="mb-5 text-lg font-medium">Підтвердження акаунту</h1>
+                        <h3 className="mb-3 text-left text-base">Дякуємо за реєстрацію! </h3>
+                        <p className="text-left text-base">Будь ласка, перевірте свою електронну пошту та підтвердіть свою реєстрацію,
+                            щоб розпочати користуватися нашим сервісом. Не забудьте перевірити папку «Спам»,
+                            якщо лист не зʼявиться в основній скринці.<br/> З повагою, команда LYCHYLNYK.</p>
+                        <p className="text-left text-base mt-4">Надіслати меня лист повторно</p>
+                    </div>
+                    <div className="pt-2 w-44 mx-auto">
+                        <Button type="button" label={'Ok'} cssType={'primary'} onClick={() => setModalActive(false)} />
+                    </div>
+                </Modal> :
             <form className="space-y-2" action="#" autoComplete="off" onSubmit={Submit}>
                 <InputField
                     type={'email'}
