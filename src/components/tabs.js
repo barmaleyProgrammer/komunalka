81acb01cc541400564fe0ca8839d0511b73b9f77
Volodymyr Grebeniuk <grebenyukvd@gmail.com>
2023-10-04 11:25:26 +0300

    fixed addAddress (form)

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -1,10 +1,31 @@
 import { NavLink } from "react-router-dom";
 const Tabs = () => {
+    const links = [
+        {
+            url: '/cabinet',
+            name: 'Мої адреси'
+        },
+        {
+            url: '/cabinet/myData',
+            name: 'Мої дані'
+        },
+        {
+            url: '#',
+            name: 'Оповіщення'
+        },
+    ];
     return (
         <ul className="flex flex-row p-0 m-0 font-medium rounded-lg bg-gray-50 space-x-8 border-0">
-            <li><NavLink to="/cabinet" className="py-2.5 px-5 text-sm font-medium rounded text-black_figma bg-[#CEDDE9]">Мої адреси</NavLink></li>
-            <li><NavLink to="/cabinet/myData" className="py-2.5 px-5 text-sm font-medium rounded text-black_figma bg-[#CEDDE9]">Мої дані</NavLink></li>
-            <li><NavLink  to="#" className="py-2.5 px-5 text-sm font-medium rounded text-black_figma bg-[#CEDDE9]">Оповіщення</NavLink></li>
+            { links.map((item, key) => {
+                const active = (item.url === window.location.pathname);
+                return (
+                    <li key={key}>
+                        <NavLink to={item.url} className={`py-2.5 px-5 text-sm font-medium rounded text-black_figma ${active ? 'bg-[#CEDDE9]' : ''}`}>
+                            {item.name}
+                        </NavLink>
+                    </li>
+                );
+            }) }
         </ul>
     );
 };
