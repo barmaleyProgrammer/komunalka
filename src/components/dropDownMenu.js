62b02d6b75cb483767bbb486551b5fc48fdf7c0b
Volodymyr Grebeniuk <grebenyukvd@gmail.com>
2023-10-09 19:08:10 +0300

    fixed client comments: modal confirm deleteAddress

--- src/components/dropDownMenu.js
+++ src/components/dropDownMenu.js
@@ -20,10 +20,10 @@ const DropDownMenu = (props) => {
                     <path d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z"/>
                 </svg>
             </button>
-            <div className={`absolute bg-white_figma divide-y divide-gray-100 rounded-lg shadow w-44 ${state ? '':'hidden'}`}>
+            <div className={`absolute bg-white_figma divide-y divide-gray-100 rounded-lg shadow w-56 ${state ? '':'hidden'}`}>
                 <ul className="py-2 text-sm text-gray-700">
-                    <li><NavLink to="#" onClick={props.rename} className="block px-4 py-2 hover:bg-gray-100">Перейменувати адресу</NavLink></li>
-                    <li><NavLink to="#" className="block px-4 py-2 hover:bg-gray-100">Налаштування сповішень</NavLink></li>
+                    <li><NavLink to="#" onClick={props.rename} className="block px-4 py-2 hover:bg-gray-100">Перейменувати&nbsp;адресу</NavLink></li>
+                    <li><NavLink to="#" className="block px-4 py-2 hover:bg-gray-100">Налаштування&nbsp;сповішень</NavLink></li>
                     {/*<li><NavLink to="#" onClick={props.delete} className="block px-4 py-2 hover:bg-gray-100">Видалити</NavLink></li>*/}
                     <li><NavLink to="#" onClick={props.delete} className="block px-4 py-2 hover:bg-gray-100">Видалити</NavLink></li>
                 </ul>
