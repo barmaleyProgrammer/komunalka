b6a20a5c66f392e3f3fb7708e0c1b16c391a9a63
Volodymyr Grebeniuk <grebenyukvd@gmail.com>
2023-10-16 19:51:22 +0300

    fixed client comments:

--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -1,11 +1,19 @@
+import { useEffect } from 'react';
 import './modal.css';
 import icon_close from '../../img/icon_close.svg';
 
 // https://www.youtube.com/watch?v=i2Yf7JZonB4
 const Modal = ({close, children}) => {
+    useEffect( () => {
+        document.body.style.overflow = 'hidden';
+        return () => {
+            document.body.style.overflow = 'auto';
+        }
+    }, []);
+
     return (
-        <div className="modal active" onClick={close}>
-            <div className="modal__content active" onClick={e => e.stopPropagation()}>
+        <div className="modal" onClick={close}>
+            <div className="modal__content" onClick={e => e.stopPropagation()}>
                 <div className="icon_close" onClick={close}>
                     <img src={icon_close} alt=""/>
                 </div>
