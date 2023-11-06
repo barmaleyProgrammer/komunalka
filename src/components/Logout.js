72362b8fd964050e931dc3e57b62abf762d435b5
Volodymyr Grebeniuk <grebenyukvd@gmail.com>
2023-11-06 18:33:41 +0200

    fixing client comments:

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -33,9 +33,9 @@ const Logout = () => {
         <div className="relative" id="logOutBtn">
             <button className="py-2.5 px-2 text-sm font-medium rounded w-full text-white_figma bg-yellow_figma inline-flex items-center" type="button">
                 {`${state.user.firstName} ${state.user.lastName}`}
-                <svg className="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4"/>
-                </svg>
+                {/*<svg className="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">*/}
+                {/*    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4"/>*/}
+                {/*</svg>*/}
             </button>
             <div className={`z-30 absolute bg-white_figma divide-y divide-gray-100 rounded-lg shadow w-44 ${show ? '':'hidden'}`} onClick={() => setShow(false)}>
                 <ul className="py-2 text-sm text-gray-700">
