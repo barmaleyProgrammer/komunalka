79e221839f91a88bc5be880388c312cc989445e3
Volodymyr Grebeniuk <grebenyukvd@gmail.com>
2023-11-14 16:44:07 +0200

    fixing client comments:

--- src/components/counterForms.js
+++ src/components/counterForms.js
@@ -2,6 +2,8 @@ import { useContext } from 'react';
 import { Context } from "../store";
 import InputField from "./inputField";
 import Button from "./button";
+import Neiron from "./../img/neiron.png"
+import icon_question from "./../img/icon_question.png"
 
 const CounterForms = ({counters, setCounters, Save}) => {
     const [state] = useContext(Context);
@@ -25,7 +27,7 @@ const CounterForms = ({counters, setCounters, Save}) => {
                     return (item.serviceType == state.serviceType);
                 }).map((item, key) => {
                     return (
-                        <div key={key} className="my-4 p-4 flex gap-x-4 rounded-lg border border-borderColor w-full">
+                        <div key={key} className="relative my-4 p-4 flex gap-x-4 rounded-lg border border-borderColor w-full">
                             <div className="w-2/3">
                                 <ul>
                                     <li className="text-xs">Лічильник №{item.counterNo}</li>
@@ -50,6 +52,8 @@ const CounterForms = ({counters, setCounters, Save}) => {
                                     onChange={(e) => handleInputChange(e, item)}
                                 />
                             </div>
+                            <img className="w-[32px] h-[31px] my-auto mr-5" src={Neiron} alt=""/>
+                            <img className="icon_question w-[24px] h-[24px]" src={icon_question} alt=""/>
                         </div>
                     );
                 })
