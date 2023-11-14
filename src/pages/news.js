0022bf2c3cff2cb6207f7de5b0f309aaf3d0a878
Volodymyr Grebeniuk <grebenyukvd@gmail.com>
2023-11-14 16:10:38 +0200

    fixing client comments:

--- src/pages/news.js
+++ src/pages/news.js
@@ -6,21 +6,36 @@ import Pagination from "../components/pagination";
 import CurrentNews from "./currentNews";
 
 const list_imgs = [
-    { image: '/news/News1.png' },
-    { image: '/news/News2.png' },
-    { image: '/news/News3.png' },
-    { image: '/news/News4.png' },
-    { image: '/news/News5.png' },
-    { image: '/news/News6.png' },
-    { image: '/news/News7.png' },
-    { image: '/news/News8.png' },
-    { image: '/news/News9.png' },
-    { image: '/news/News10.png' },
-    { image: '/news/News11.png' },
-    { image: '/news/News12.png' },
-    { image: '/news/News13.png' },
-    { image: '/news/News14.png' },
-    { image: '/news/News15.png' },
+    { image: '/news/News1.png',
+        news: '1'},
+    { image: '/news/News2.png',
+        news: '2'},
+    { image: '/news/News3.png',
+        news: '3' },
+    { image: '/news/News4.png',
+        news: '4' },
+    { image: '/news/News5.png',
+        news: '5' },
+    { image: '/news/News6.png',
+        news: '6' },
+    { image: '/news/News7.png',
+        news: '7' },
+    { image: '/news/News8.png',
+        news: '8' },
+    { image: '/news/News9.png',
+        news: '9' },
+    { image: '/news/News10.png',
+        news: '10' },
+    { image: '/news/News11.png',
+        news: '11' },
+    { image: '/news/News12.png',
+        news: '12' },
+    { image: '/news/News13.png',
+        news: '13' },
+    { image: '/news/News14.png',
+        news: '14' },
+    { image: '/news/News15.png',
+        news: '15' },
 ];
 
 const breadCrumbs = [
@@ -53,6 +68,7 @@ const News = () => {
                         return (
                             <div key={key}>
                                 <NavLink to={`/news/${key}`}>
+                                    <h2>новина № {item.news}</h2>
                                     <img className="rounded-lg mt-4" src={item.image} alt=""/>
                                 </NavLink>
                             </div>
