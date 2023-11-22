bdd0da4b725119e5938a5d383f2c38a6631a34f7
Volodymyr Grebeniuk <grebenyukvd@gmail.com>
2023-11-22 10:26:58 +0200

    fixing client comments:

---
+++ src/components/toRead.js
@@ -0,0 +1,7 @@
+import rectangle from "../img/rectangle.png";
+import rectangle_ from "../img/Rectangle_.png";
+import {NavLink} from "react-router-dom";
+import NewsList from "./news"
+import {useState} from "react";
+import arrowLeftCurrentNews from "../img/arrowLeftCurrentNews.svg";
+import arrowRightCurrentNews from "../img/arrowRightCurrentNews.svg";
