23f30a3a7035ede265c09d6d7305718e76a18763
Volodymyr Grebeniuk <grebenyukvd@gmail.com>
2023-11-28 18:17:22 +0200

    fixing client comments: pin code

--- src/pages/auth/resetPassword.js
+++ src/pages/auth/resetPassword.js
@@ -1,21 +1,18 @@
-import { useState } from "react";
+import {useContext, useState} from "react";
 import InputField from "../../components/inputField";
-import { useNavigate } from "react-router-dom";
 import logo_lichylnyk from "../../img/logo_lichylnyk.svg";
 import Button from "../../components/button";
 import api from "../../api";
-import Modal from "../../components/modal/modal";
+import {Context} from "../../store";
 
 const ResetPasswordRequest = ( {close} ) => {
-    const [validateFlag, setValidateFlag] = useState(false);
+    const [,dispatch] = useContext(Context);
+
     const [modalActive, setModalActive] = useState(false);
-    // const [showResetPass, setShowResetPass] = useState(false);
-    const navigate = useNavigate();
     const [form, setForm] = useState({
         email: (process.env.NODE_ENV === 'development') ? 'grebenyukvd@gmail.com' : '',
         source: (process.env.NODE_ENV === 'development') ? 'localhost_3000' : '2',
     });
-    const [formError, setFormError] = useState('');
 
     const handleInputChange = (event) => {
         const { name, value } = event.target;
@@ -26,34 +23,25 @@ const ResetPasswordRequest = ( {close} ) => {
     };
     const Submit = async (event) => {
         event.preventDefault();
-        try {
-            const result = await api.resetPasswordRequest(form.email, form.source);
-            if (result.status === 200) {
-                // setValidateFlag(true);
-                setModalActive(true);
-            }
-            // navigate('/');
-        }
-
-        catch (e) {
-                setFormError(e);
-            }
+            await api.resetPasswordRequest(form.email, form.source).then((result) => {
+                if (result.status === 200) {
+                    setModalActive(true);
+                }
+            }).catch((error) => {
+                dispatch({ type: 'error', payload: error });
+            });
     };
 
     return (
         <div className="mt-10 p-10 w-[464px]">
             <img src={logo_lichylnyk} className="h-16 mb-8 mx-auto" alt="Flowbite Logo" />
             <h4 className="text-black_figma text-center">Відновлення пароля</h4>
-            <div className="text-xs text-red-900 text-center">{formError}</div>
             { modalActive ?
                 <div>
                     <div className="text-xs text-black_figma p-5 text-center w-full">
-                        {/*<h1 className="mb-5 text-lg font-medium">Підтвердження акаунту</h1>*/}
-                        {/*<h3 className="mb-3 text-left text-base">Дякуємо за реєстрацію! </h3>*/}
                         <p className="text-left text-base">Будь ласка, перевірте свою електронну пошту, та натисніть на посилання.
                             Не забудьте перевірити папку «Спам», якщо лист не зʼявиться в основній скринці.
                             <br/> З повагою, команда LYCHYLNYK.</p>
-                        {/*<p className="text-left text-base mt-4 underline underline-offset-4">Надіслати мені лист повторно</p>*/}
                     </div>
                     <div className="pt-2 w-44 mx-auto">
                         <Button type="button" label={'Ok'} cssType={'primary'} onClick={close} />
