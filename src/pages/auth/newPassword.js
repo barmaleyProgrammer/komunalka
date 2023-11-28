e9fc0c6621eea7646ba5e05d5ea67db2739b66a3
Volodymyr Grebeniuk <grebenyukvd@gmail.com>
2023-11-28 18:41:00 +0200

    fixing client comments: ApiError newPassword

--- src/pages/auth/newPassword.js
+++ src/pages/auth/newPassword.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import {useContext, useState} from "react";
 import { useNavigate } from "react-router-dom";
 import InputField from "../../components/inputField";
 import logo_lichylnyk from "../../img/logo_lichylnyk.svg";
@@ -7,8 +7,10 @@ import Button from "../../components/button";
 import api from "../../api";
 import Modal from "../../components/modal/modal";
 import CheckPassword from "../../components/checkPassword/checkPassword";
+import {Context} from "../../store";
 
 const NewPassword = ({ close }) => {
+    const [, dispatch] = useContext(Context);
     const url = new URL(window.location);
     const navigate = useNavigate();
     const [type, setTape] = useState('password');
@@ -20,7 +22,6 @@ const NewPassword = ({ close }) => {
         password: (process.env.NODE_ENV === 'development') ? 'Test_Drive5' : '',
         token: url.searchParams.get('code') || ''
     });
-    const [formError, setFormError] = useState('');
     const handleInputChange = (event) => {
         const value = (event.target.type === 'checkbox') ? event.target.checked : event.target.value;
         setForm((prevProps) => ({
@@ -31,67 +32,67 @@ const NewPassword = ({ close }) => {
 
     const Submit = async (event) => {
         event.preventDefault();
-        await api.newPassword(form);
-        navigate('/');
-    };
-
-    const closeModal = () => {
-        setModalActive(false)
-        navigate('/');
+        await api.newPassword(form)
+            .catch((error) => {
+                dispatch({type: 'error', payload: error});
+                navigate('/');
+            });
     }
-    const togglePassInput = () => {
-        if (type === 'password') {
-            setTape('text')
-        } else {
-            setTape('password')
+        const closeModal = () => {
+            setModalActive(false)
+            navigate('/');
         }
-    }
-    return (
-        <div>
-            {   modalActive && (
-                <Modal close={() => closeModal()}>
-                    {/*<div className="mt-20 mb-20 p-10 mx-auto rounded-lg shadow-myCustom sm:w-3/4 md:w-3/4 lg:w-3/4 xl:w-1/3 max-w-[450px]">*/}
-                    <div className="p-10 w-full">
-                        <img src={logo_lichylnyk} className="h-16 mb-8 mx-auto" alt="" />
-                        <h4 className="text-black_figma text-center">Новий пароль</h4>
-                        <div className="text-red-950 text-center">{ formError }</div>
-                        <form className="space-y-2" onSubmit={Submit}>
-                            <InputField
-                                label={'Email'}
-                                type={'email'}
-                                placeholder={'Введіть свій email'}
-                                name={'email'}
-                                // required={true}
-                                readOnly={true}
-                                value={form.email}
-                                autoComplete="off"
-                                onChange={handleInputChange}
-                            />
-                            <div className={'relative'}>
+        const togglePassInput = () => {
+            if (type === 'password') {
+                setTape('text')
+            } else {
+                setTape('password')
+            }
+        }
+        return (
+            <div>
+                {modalActive && (
+                    <Modal close={() => closeModal()}>
+                        <div className="p-10 w-full">
+                            <img src={logo_lichylnyk} className="h-16 mb-8 mx-auto" alt=""/>
+                            <h4 className="text-black_figma text-center">Новий пароль</h4>
+                            <form className="space-y-2" onSubmit={Submit}>
                                 <InputField
-                                    label={'Пароль'}
-                                    type={type}
-                                    name={'password'}
-                                    required={true}
-                                    value={form.password}
+                                    label={'Email'}
+                                    type={'email'}
+                                    placeholder={'Введіть свій email'}
+                                    name={'email'}
+                                    // required={true}
+                                    readOnly={true}
+                                    value={form.email}
                                     autoComplete="off"
-                                    onFocus={() => setInputPassFlag(true)}
-                                    onBlur={() => setInputPassFlag(false)}
                                     onChange={handleInputChange}
                                 />
-                                { inputPassFlag ? <CheckPassword password={form.password} /> : <></> }
-                                <div onClick={togglePassInput} className="eye-ico cursor-pointer">
-                                    <img src={eye} alt="" />
+                                <div className={'relative'}>
+                                    <InputField
+                                        label={'Пароль'}
+                                        type={type}
+                                        name={'password'}
+                                        required={true}
+                                        value={form.password}
+                                        autoComplete="off"
+                                        onFocus={() => setInputPassFlag(true)}
+                                        onBlur={() => setInputPassFlag(false)}
+                                        onChange={handleInputChange}
+                                    />
+                                    {inputPassFlag ? <CheckPassword password={form.password}/> : <></>}
+                                    <div onClick={togglePassInput} className="eye-ico cursor-pointer">
+                                        <img src={eye} alt=""/>
+                                    </div>
                                 </div>
-                            </div>
-                            <Button type="submit" label={'Зберегти'} cssType={'primary'} />
-                        </form>
-                    </div>
-                </Modal>
+                                <Button type="submit" label={'Зберегти'} cssType={'primary'}/>
+                            </form>
+                        </div>
+                    </Modal>
                 )
-            }
-        </div>
-    );
-};
+                }
+            </div>
+        );
+    };
 
 export default NewPassword;
