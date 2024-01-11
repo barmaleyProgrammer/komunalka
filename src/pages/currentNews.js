b33009f1fb461d0a0993e1ca6b98ebb87a10e656
Volodymyr Grebeniuk <grebenyukvd@gmail.com>
2024-01-11 17:18:53 +0200

    current news convert html from cms

--- src/pages/currentNews.js
+++ src/pages/currentNews.js
@@ -15,15 +15,9 @@ const CurrentNews = ({ id }) => {
             <p className="text-sm font-extralight mt-4">{moment(news.publish_date).format('DD.MM.YYYY')}</p>
             <img className="mt-4" src={news.banner} alt=""/>
             <div className="w-[956px] mx-auto">
-                {news.body}
-                <img className="mt-4" src={news.main_photo} alt=""/>
-                <h2 className="text-xl mt-2">Як передати показання лічильника води?</h2>
-                <p className="mt-4 font-light">Є кілька способів. Найзручніший варіант — зняти показання
-                    лічильника холодної води та приладу для обліку гарячої та передати дані постачальнику послуги
-                    через інтернет за допомогою сервісу LICHYLNYK. Сервіс надає змогу передавати інформацію про
-                    фактичне споживання води, газу та електроенергії в режимі он-лайн.</p>
-                <p className="mt-8 font-light">Ця функція доступна в особистому кабінеті.
-                    Щоб скористатися послугою, слід пройти просту процедуру реєстрації на сайті Лічильник.</p>
+                <div className="agreement">
+                <div dangerouslySetInnerHTML = {{ __html: news.body }} ></div>
+                </div>
             </div>
             <div className="w-[305px] h-12 mx-auto mt-5 mb-5">
                 <NavLink to="/cabinet" className="py-3.5 px-5 mr-2 mb-2 text-sm rounded text-white_figma bg-yellow_figma">
